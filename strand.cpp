b9c17406dc4044c2021d83d030252c79ff59b728
Tyrine Gitau <tyrinegitau265@gmail.com>
2026-04-11 12:38:56 +0300

    adding the main function and elements to the created list

--- strand.cpp
+++ strand.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-oid strandSort(list<int> &ip, list<int> &op)
+/* strand sort is implemnted in stl::list because it recquires frequent operations in the middle of the list 
+which would otherwise be expensive if an array is used*/
+void strandSort(list<int> &ip, list<int> &op)
 {
     
     if (ip.empty())
@@ -13,7 +15,8 @@ oid strandSort(list<int> &ip, list<int> &op)
     
     for (list<int>::iterator it = ip.begin(); it != ip.end(); ) {
 
-       
+      /* this section of code implements the ascending direction of sorting 
+      the list*/ 
         if (*it > sublist.back()) {
             sublist.push_back(*it);   
             it = ip.erase(it);
@@ -25,5 +28,28 @@ oid strandSort(list<int> &ip, list<int> &op)
   op.merge(sublist);
     
     strandSort(ip, op);
+}
+
+int main(void)
+{
+    list<int> ip;
+ip.push_back(10);
+ip.push_back(5);
+ip.push_back(30);
+ip.push_back(40);
+ip.push_back(2);
+ip.push_back(4);
+ip.push_back(9);
+
+   
+    list<int> op;
+
+    
+    strandSort(ip, op);
+for (list<int>::iterator it = op.begin(); it != op.end(); ++it) {
+    int x = *it;
+    cout << x << " ";
+}
+    return 0;
 }
 
